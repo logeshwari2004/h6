29449dccecb47fd0bee8cb072142699c1189eaf2
logeshwari <94211349+logeshwari2004@users.noreply.github.com>
2024-07-23 22:27:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    display.innerText = display.innerText.slice(0, -1);
+    if (display.innerText === '') {
+        display.innerText = '0';
+    }
+}
+
+function appendNumber(number) {
+    if (display.innerText === '0') {
+        display.innerText = number;
+    } else {
+        display.innerText += number;
+    }
+}
+
+function calculate() {
+    try {
+        display.innerText = eval(display.innerText.replace('×', '*').replace('÷', '/'));
+    } catch {
+        display.innerText = 'Error';
+    }
+}
